4b4186ab2f0ca6ffa83934dc5e6323f89b076bf6
Gustav Corpas <56586922+gustavcorpas@users.noreply.github.com>
2025-03-15 19:32:04 +0100

    add tripping shader

---
+++ Levels/tile_map_layer.gd
@@ -0,0 +1,16 @@
+extends TileMapLayer
+
+static var fkd: float = 0.0
+var increase: float = 0.05
+
+func _ready() -> void:
+	material.set_shader_parameter("intensity", fkd)
+	Gridmanager.SceneChange.connect(increase_fukd)
+
+func increase_fukd() -> void:
+	fkd += increase
+	print("fdk", fkd)
+	material.set_shader_parameter("intensity", fkd)
+	
+	
+	
